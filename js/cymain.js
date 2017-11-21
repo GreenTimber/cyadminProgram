522c5dda5577f916494eac0bae8075f08195bfcd
GreenTimber <513340030@qq.com>
2017-11-21 15:18:52 +0800

    Revert "hello world"

--- js/cymain.js
+++ js/cymain.js
@@ -66,7 +66,6 @@ function addtab(menuid,tabname,taburl){
 // 关闭tab
 function closetab(id){
     $("#myTab").find("li").each(function () {
-
             if($(".cytab"+id+"").index()==$(this).index()&&$(this).hasClass("active")==true){
                      if($(this).index()+1==$("#myTab").find("li").length){
                          $(this).prev("li").addClass("active");
@@ -80,13 +79,11 @@ function closetab(id){
                          $(".cytab"+id+"").remove();
                      }
                 }
-
             if($(".cytab"+id+"").index()==$(this).index()&&$(this).hasClass("active")==false){
                 $($(".cytab"+id+"").children().attr("href")).remove();
                 $(".cytab"+id+"").remove();
                 }
         });
-
 }
 //手机端显示菜单
 $('.cymobile-block-menu').click(function () {
